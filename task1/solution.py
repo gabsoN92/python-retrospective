4f576cb0d79000e350814fddb69b254e27f53c1b
Gabriel Gatev <gabogatev@gmail.com>
2013-04-28 20:10:22 +0300

    new version of 1st homework

--- task1/solution.py
+++ task1/solution.py
@@ -1,61 +1,11 @@
-def what_is_my_sign(day,month):
-    if month == 3:
-        if day >= 0 and day <= 20: 
-            return "Риби"
-        elif day >= 21 and day <= 31: 
-            return "Овен"
-    if month == 4:
-        if day >= 0 and day <= 20: 
-            return "Овен"
-        elif day >= 21 and day <= 30: 
-            return "Телец"
-    if month == 5:
-        if day >= 0 and day <= 20: 
-            return "Телец"
-        elif day >= 21 and day <= 31: 
-            return "Близнаци"
-    if month == 6:
-        if day >= 0 and day <= 20: 
-            return "Близнаци"
-        elif day >= 21 and day <= 30: 
-            return "Рак"
-    if month == 7:
-        if day >= 0 and day <= 21: 
-            return "Рак"
-        elif day >= 22 and day <= 31: 
-            return "Лъв"
-    if month == 8:
-        if day >= 0 and day <= 22: 
-            return "Лъв"
-        elif day >= 23 and day <= 31: 
-            return "Дева"
-    if month == 9:
-        if day >= 0 and day <= 22: 
-            return "Дева"
-        elif day >= 23 and day <= 30: 
-            return "Везни"
-    if month == 10:
-        if day >= 0 and day <= 22: 
-            return "Везни"
-        elif day >= 23 and day <= 31: 
-            return "Скорпион"
-    if month == 11:
-        if day >= 0 and day <= 21: 
-            return "Скорпион"
-        elif day >= 22 and day <= 30: 
-            return "Стрелец"
-    if month == 12:
-        if day >= 0 and day <= 21: 
-            return "Стрелец"
-        elif day >= 22 and day <= 31: 
-            return "Козирог"
-    if month == 1:
-        if day >= 0 and day <= 19: 
-            return "Козирог"
-        elif day >= 20 and day <= 31: 
-            return "Водолей"
-    if month == 2:
-        if day >= 0 and day <= 18: 
-            return "Водолей"
-        elif day >= 19 and day <= 29: 
-            return "Риби"
+SIGNS = ['Козирог', 'Водолей', 'Риби', 'Овен', 'Телец', 'Близнаци',
+         'Рак', 'Лъв', 'Дева', 'Везни', 'Скорпион', 'Стрелец', 'Козирог']
+DATES = [19, 18, 20, 20, 20, 20, 21, 22, 22, 22, 21, 21]
+
+
+def what_is_my_sign(day, month):
+    if day <= DATES[month - 1]:
+        return SIGNS[month - 1]
+    else:
+        return SIGNS[month]
+
