3a08ac3ebab12b834590f2ae652ae74923442c8e
Gabriel Gatev <gabogatev@gmail.com>
2013-04-28 20:24:25 +0300

    added new function and some changes to the old

--- task2/solution.py
+++ task2/solution.py
@@ -1,9 +1,12 @@
+from collections import deque
+
+
 def groupby(func, seq):
     result = {}
-    
+
     for x in seq:
         result[func(x)] = [y for y in seq if func(y) == func(x)]
-    
+
     return result
 
 
@@ -13,7 +16,34 @@ def zip_with(func, *iterables):
         yield func(*next(zip_iterables))
 
 
+def composer(f, g):
+    return lambda x: f(g(x))
+
+
 def iterate(func):
     yield lambda x: x
-    for y in iterate(func):
-        yield lambda x: func(y(x))
+    composition = func
+    while True:
+        yield composition
+        composition = composer(func, composition)
+
+
+def cache(func, cache_size):
+    values = {}
+    cache_order = deque()
+
+    def func_cashed(*args):
+        if args in values:
+            return values[args]
+
+        result = func(*args)
+
+        if cache_size:
+            if len(values) == cache_size:
+                values.pop(cache_order.popleft())
+            cache_order.append(args)
+            values[args] = result
+
+        return result
+    return func_cashed
+
